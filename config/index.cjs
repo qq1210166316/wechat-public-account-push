6b383dfff0a6944a0af456ba0e684f467ec0e294
qq1210166316 <85779441+qq1210166316@users.noreply.github.com>
2023-02-01 15:35:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oWjtk6un8kgqSbDWzg330tAB7plQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'TxJgjHLbcBMkXszDNuOckn9NZ1Snv943nINavhlvFlM',
+      useTemplateId: 'gyoeKBpYxzAcwbwUZXp1sVoDW4NmRz5z0zhksaq9Ou0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-03',
       festivals: [
