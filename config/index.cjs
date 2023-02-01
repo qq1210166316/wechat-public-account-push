fbca2edd2dd49ec438cb59ec67e2f368181e7f20
qq1210166316 <85779441+qq1210166316@users.noreply.github.com>
2023-02-01 15:17:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,15 +25,15 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'TxJgjHLbcBMkXszDNuOckn9NZ1Snv943nINavhlvFlM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '2000', date: '08-19',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '阿龙', year: '2001', date: '08-12',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '01-30',
